9810d83c9b1cd5549f2f128ab5712e24bdb4bd81
khanzohan <zoho12@live.com>
2019-11-14 12:25:54 -0500

    Updates

---
+++ precache-manifest.09de993746a32f54e9222372e24edb7d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47a13a8c9431d6b0eeaf45c076a52d5f",
+    "url": "/ReactCards/index.html"
+  },
+  {
+    "revision": "1dc8b45c284c6fb60305",
+    "url": "/ReactCards/static/css/main.02b59776.chunk.css"
+  },
+  {
+    "revision": "b2b7a11a1485fbeb486f",
+    "url": "/ReactCards/static/js/2.f6046a0a.chunk.js"
+  },
+  {
+    "revision": "1dc8b45c284c6fb60305",
+    "url": "/ReactCards/static/js/main.7e60769a.chunk.js"
+  },
+  {
+    "revision": "4dbe8e734f783198b2f7",
+    "url": "/ReactCards/static/js/runtime-main.8f4a2744.js"
+  }
+]);
